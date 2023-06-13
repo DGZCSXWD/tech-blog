f48adde33f369729e1706e4a39e3fb7a47d8f196
DGZCSXWD <fjjychenrui@gmail.com>
2023-06-13 21:57:02 +1000

    update routes

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -12,15 +12,36 @@ router.get("/", async (req, res) => {
           attributes: ["username"],
         },
       ],
+      order: [["createdAt", "DESC"]],
     });
 
-    const blogs = blogData.map((blog) => blog.get({ plain: true }));
+    console.log("Blog data fetched:", blogData);
+
+    const blogs = blogData.map((blog) => {
+      const { id, title, content, user_id, user, createdAt } = blog.get({
+        plain: true,
+      });
+
+      console.log("User in blog data:", user);
+
+      return {
+        id,
+        title,
+        content,
+        user_id,
+        usernamee: user.username,
+        createdAt: createdAt.toLocaleString("en-AU"),
+      };
+    });
+
+    console.log("Blogs:", blogs);
 
     res.render("homepage", {
       blogs,
       logged_in: req.session.logged_in,
     });
   } catch (err) {
+    console.error("An error occurred:", err);
     res.status(500).json(err);
   }
 });
@@ -42,12 +63,25 @@ router.get("/blog/:id", withAuth, async (req, res) => {
               attributes: ["username"],
             },
           ],
-          attributes: ["comment_text"],
+          attributes: ["comment_text", "createdAt"], // add createdAt here
         },
       ],
     });
 
-    const blog = blogData.get({ plain: true });
+    let blog = blogData.get({ plain: true });
+
+    blog.createdAt = blog.createdAt.toLocaleString("en-AU"); // transform blog date to local string
+
+    blog.comments = blog.comments.map((comment) => {
+      const { id, comment_text, user, createdAt } = comment;
+      return {
+        id,
+        comment_text,
+        user,
+        createdAt: createdAt.toLocaleString("en-AU"),
+      };
+    });
+
     const isAuthor = blog.user_id === req.session.user_id;
 
     res.render("blog", {
